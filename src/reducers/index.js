d77200fb40a353b8dcccbfacb0cd54284dc1b267
jisooismine <15927338773@163.com>
2022-05-15 00:03:55 +0800

    本次提交 完成了登录注册功能 后端同步数据库 google身份验证还存在问题 需要继续修改

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-
-import posts from './posts';
-//combineReducers用于对多个reducer进行整合 下面这种写法要求state的属性名必须与子reducer同名 
-//根据state的key去执行相应的子reducer
-export default combineReducers({
-    posts,
-})
